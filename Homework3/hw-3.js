2c47f731176ce0d6a7bab4861c9939019f31c77d
*Lineilli* <*dark.heart808@yandex.ru*>
2023-04-17 13:54:28 +0600

    Changed the file hw-3

--- Homework3/hw-3.js
+++ Homework3/hw-3.js
@@ -1,6 +1,6 @@
 // задание 1
 
-let password = String(prompt('Введите пароль'));
+let password = prompt('Введите пароль');
 
 if (password === 'пароль') {
     console.log('Пароль введен верно');
@@ -20,7 +20,8 @@ if (c > 0 && c < 10) {
 
 // задание 3
 
-let d, e = Number(prompt('Введите число'));
+let d = Number(prompt('Введите число'));
+let e = Number(prompt('Введите число'));
 
 if (d > 100 || e > 100) {
     console.log('Верно');
@@ -32,43 +33,68 @@ if (d > 100 || e > 100) {
 
 let a = '2';
 let b = '3';
-alert(parseInt(a) + parseInt(b));
+alert(Number(a) + Number(b));
 
 // задание 5
 
 let monthNumber = Number(prompt('Введите номер месяца'));
 
 switch (monthNumber) {
-    case 1, 2, 12:
+    case 1:
         console.log('Зима');
         break;
-    case 3, 4, 5:
+    case 2:
+        console.log('Зима');
+        break;
+    case 12:
+        console.log('Зима');
+        break;
+    case 3:
         console.log('Весна');
         break;
-    case 6, 7, 8:
+    case 4:
+        console.log('Весна');
+        break;
+    case 5:
+        console.log('Весна');
+        break;
+    case 6:
         console.log('Лето');
         break;
-    case 9, 10, 11:
+    case 7:
+        console.log('Лето');
+        break;
+    case 8:
+        console.log('Лето');
+        break;
+    case 9:
+        console.log('Осень');
+        break;
+    case 10:
+        console.log('Осень');
+        break;
+    case 11:
         console.log('Осень');
         break;
     default: console.log('В году 12 месяцев');
-    break;
-    }
+        break;
+}
 
 // задание 7
 
 let answer = Number(prompt('Пожалуйста, введите любое число'));
 
-if ("answer" > 0) {
-   console.log('Верно, вы ввели число'); 
+if (!isNaN(answer)) {
+    console.log('Верно, вы ввели число');
 } else {
     console.log(NaN);
 }
-if ("answer" / 2 => !("answer" % 2)) {
+if (answer / 2 => !(answer % 2)) {
     alert('Число четное');
 } else {
     alert('Число нечетное');
 }
+
 
 
 
